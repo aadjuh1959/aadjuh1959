13a6d34bfab1857f0c2dcf450ad18857292e14d5
aadjuh1959 <aadfouw@hotmail.com>
2017-02-19 15:51:52 +0100

    Add files via upload

---
+++ -New userscript-.user.js
@@ -0,0 +1,22 @@
+// ==UserScript== aadjuh
+// @name         New Userscript aadjuh
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the function aadjuh() {
+
+
+
+aadjuh.prototype = {
+
+};
+
+// @author       You
+// @match        http://*/*world of warships
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	// Your code here...
+})();
